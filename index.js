a7a964efc4f981d2e8d415baa8578fcd9ffb7b80
Hafid258 <6504305001340@student.sru.ac.th>
2025-03-07 14:35:02 +0700

    file test

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>เลือกหน้าที่ต้องการ</h1><a href="/hello-word">ไปที่ Hello World</a><a href="/S-A">ไปที่ S-A</a><a href="/5">ไปที่ สวัสดีวันศุกร์</a>');
+0
+});
+
+app.get('/hello-word', (req, res) => {
+    res.send('Hello World! <br>Surakiert Namsi <br> Anussara Khongthong');
+
+});
+
+app.get('/S-A', (req, res) => {
+    res.send('Surakiert Namsi <br> Anussara Khongthong');
+
+});
+
+app.get('/5', (req, res) => {
+    res.send(`
+        <html>
+            <head>
+                <style>
+                    body {
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        height: 100vh;
+                        margin: 0;
+                        font-family: Arial, sans-serif;
+                    }
+                    h1 {
+                        text-align: center;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1>สวัสดีวันศุกร์</h1>
+            </body>
+        </html>
+    `);
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
